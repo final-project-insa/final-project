c4b5883903b3933f6ea1e3b97c73eccbccc37030
Kid - NGUYEN Le Ngan Ha <kid.nguyenlenganha@gmail.com>
2023-01-08 02:07:02 +0100

    WIP

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,6 +77,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "knl2.users",
+    "knl2.ticket",
+    "knl2.souvenir",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
